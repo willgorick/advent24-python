b7ff5989a50d5302cd6a9dd35c992bae972c2dfb
willgorick <wig4da@virginia.edu>
2024-12-06 08:49:45 -0500

    optimize day 6 part 2 a bit

--- day6/solution.py
+++ day6/solution.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 DIR_MAP = {"^": (-1, 0), "v": (1, 0), ">": (0, 1), "<": (0, -1)}
 NEXT_DIR = {"^": ">", ">": "v", "v": "<", "<": "^"}
 ROW, COLS = None, None
+ORIGINAL_PATH = set()
 
 
 def part1(submit: bool):
@@ -33,38 +34,28 @@ def part2(submit: bool):
 
 def _solve1(grid: List[List[str]]):
     curr_loc, grid = _setup(grid)
-    visited_set = set()
-    travel_direction = "^"
-
-    while True:
-        visited_set.add(curr_loc)
-        move = DIR_MAP[travel_direction]
-        next_loc = (curr_loc[0]+move[0], curr_loc[1]+move[1])
-        # if we're about to go out of bounds, end the loop
-        if not _check_if_in_bounds(next_loc):
-            break
-        # if the next location is an obstacle, turn right 90 degrees
-        if grid[next_loc[0]][next_loc[1]] == "#":
-            travel_direction = NEXT_DIR[travel_direction]
-        # if no obstacle and inbounds, move to the new location
-        else:
-            curr_loc = next_loc
-    return len(visited_set)
+    return _original_path(curr_loc, grid)
 
 
 def _solve2(grid: List[List[str]]):
     curr_loc, grid = _setup(grid)
+    original_path = _original_path(curr_loc, grid, True)
     res = 0
-    for row in range(ROWS):
-        print(row, ROWS)
-        for col in range(COLS):
-            curr_spot = grid[row][col]
-            # don't try to place a new obstacle at the start
-            # spot or where an obstacle already is
-            if curr_spot not in {"^", "#"}:
-                grid[row][col] = "#"
-                res += _check_for_cycle(grid, curr_loc)
-                grid[row][col] = "."
+    # check every spot on the original path to determine
+    # if an obstruction will cause a cycle
+    tenth = len(original_path) // 10
+    i = 0
+    for (row, col) in original_path:
+        if i % tenth == 0:
+            print(f"{(i // tenth)*10}%")
+        i += 1
+        curr_spot = grid[row][col]
+        # don't try to place a new obstacle at the start
+        # spot or where an obstacle already is
+        if curr_spot not in {"^", "#"}:
+            grid[row][col] = "#"
+            res += _check_for_cycle(grid, curr_loc)
+            grid[row][col] = "."
 
     return res
 
@@ -95,17 +86,35 @@ def _check_for_cycle(grid: List[List[str]], curr_loc: tuple):
     return 0
 
 
+def _original_path(curr_loc, grid, return_path=False):
+    visited_set = set()
+    travel_direction = "^"
+
+    while True:
+        visited_set.add(curr_loc)
+        move = DIR_MAP[travel_direction]
+        next_loc = (curr_loc[0]+move[0], curr_loc[1]+move[1])
+        # if we're about to go out of bounds, end the loop
+        if not _check_if_in_bounds(next_loc):
+            break
+        # if the next location is an obstacle, turn right 90 degrees
+        if grid[next_loc[0]][next_loc[1]] == "#":
+            travel_direction = NEXT_DIR[travel_direction]
+        # if no obstacle and inbounds, move to the new location
+        else:
+            curr_loc = next_loc
+    return visited_set if return_path else len(visited_set)
+
+
 def _setup(grid: List[List[str]]):
     global ROWS
     global COLS
-    start = ()
     ROWS, COLS = len(grid), len(grid[0])
+    new_grid = [[x for x in line] for line in grid]
     for row in range(ROWS):
         for col in range(COLS):
-            if grid[row][col] == "^":
-                start = (row, col)
-    new_grid = [[x for x in line] for line in grid]
-    return start, new_grid
+            if new_grid[row][col] == "^":
+                return (row, col), new_grid
 
 
 def _check_if_in_bounds(loc: tuple):
