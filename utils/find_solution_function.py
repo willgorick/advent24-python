955631ad86974185fb9aaa569b61f383dad80311
Will Gorick <will.gorick@gmail.com>
2024-12-16 14:38:21 -0500

    fix error messaging

--- utils/find_solution_function.py
+++ utils/find_solution_function.py
@@ -4,13 +4,13 @@ def find_solution_function(day: int, part: int):
     try:
         solution = importlib.import_module(f"day{day}.solution")
     except ImportError:
-        print(f"Error: day{day} not found.")
+        print(f"Error: day{day}'s solution not found.")
         exit(1)
 
     try:
         function = getattr(solution, f"part{part}")
     except AttributeError:
-        print(f"Error: part{part} not found in day{day}.")
+        print(f"Error: part{part} not found in day{day}'s solution.")
         exit(1)
 
     return function
